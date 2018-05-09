077048f936ae883a169a30c51c8b18dd6e768ab1
jcgallegor <jcgallegor@unal.edu.co>
2018-05-09 17:59:42 -0400

    Automatic commit. Done before go to queltrics

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+var map;
+var NY = {lat: 40.730610, lng: -73.956242};
+var university = {lat: 40.729055, lng: -73.996523};
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
+function initMap(){
+    map = new google.maps.Map(document.getElementById('mapContainer'), {
+        zoom: 12,
+        center: NY
     });
-})();
+    var universityMark = new google.maps.Marker({
+          position: university,
+          map: map
+        });
+}
